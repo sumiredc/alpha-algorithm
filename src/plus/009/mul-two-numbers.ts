277de8c87317dda39763a0dc53df31965d9e27d5
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2023-10-12 11:00:04 +0900

    Refactaring 009

--- src/plus/009/mul-two-numbers.ts
+++ src/plus/009/mul-two-numbers.ts
@@ -3,8 +3,8 @@ import { ListNode } from '../../lib/listnode/listnode';
 /**
  * @see https://colab.research.google.com/drive/15SWIztOXTJdY1_QLgl6s5GnQmFL1Y33G
  *
- * @time_complexity  O(nodeA^nodeB)
- * @space_complexity O(N)
+ * @time_complexity  O(N*M)
+ * @space_complexity O(N+M)
  */
 type SpaceCalResult = { val: number; nextCarryUp: number };
 
@@ -12,8 +12,8 @@ export const solve = (
     nodeA: ListNode | null,
     nodeB: ListNode | null
 ): ListNode => {
-    let head: ListNode | null = null;
-    let tail: ListNode | null = null;
+    const head: ListNode = new ListNode(-1);
+    let tail: ListNode | null = head;
     let mulCarryUp = 0;
     let padPlace = 0;
 
@@ -22,54 +22,72 @@ export const solve = (
     // ---------
     // tail-head
     while (nodeB !== null || mulCarryUp > 0) {
-        const valB = nodeB?.val ?? 0;
-        // 2回使用するため、紛失しないよう複製
-        let nodeA2 = nodeA;
-        let addCarryUp = 0;
+        // 2行目の計算処理
+        mulCarryUp = calSecondLine(nodeA, nodeB?.val ?? 0, tail, mulCarryUp);
 
-        while (nodeA2 !== null || mulCarryUp > 0 || addCarryUp > 0) {
-            const valA = nodeA2?.val ?? 0;
-            // 途中結果の乗算処理
-            const mulResult = mulPlace(valA, valB, mulCarryUp);
-            // 繰り上げが発生したら次へ回す
-            mulCarryUp = mulResult.nextCarryUp;
-
-            // 最終結果の加算処理
-            const addResult = addPlace(
-                mulResult.val,
-                tail?.next?.val ?? 0,
-                addCarryUp
-            );
-            // 繰り上げが発生したら次へ回す
-            addCarryUp = addResult.nextCarryUp;
-
-            const node = new ListNode(addResult.val);
-
-            if (tail === null) {
-                // 初回
-                head = tail = node;
-            } else {
-                // 1回目以降
-                if (tail.next === null) {
-                    // 次の位が未計算ならnodeをそのまま採用
-                    tail.next = node;
-                } else {
-                    // 次の位がすでに計算済みであれば、valのみ書き換え
-                    tail.next.val = node.val;
-                }
-                // 1つ進める
-                tail = tail.next;
-            }
-
-            nodeA2 = nodeA2?.next ?? null;
-        }
+        // 次のBを計算対象とする
         nodeB = nodeB?.next ?? null;
-        tail = getNodeList(head!, padPlace);
+        tail = getNodeList(head.next!, padPlace);
         // tail-headを1桁ずらす
         padPlace++;
     }
 
-    return head!;
+    return head.next!;
+};
+
+// 2行目の乗算処理と結果の加算処理
+export const calSecondLine = (
+    nodeA: ListNode | null,
+    valB: number,
+    tail: ListNode | null,
+    mulCarryUp: number
+) => {
+    let addCarryUp = 0;
+    while (nodeA !== null || mulCarryUp > 0 || addCarryUp > 0) {
+        const valA = nodeA?.val ?? 0;
+        // 途中結果の乗算処理
+        const mulResult = mulPlace(valA, valB, mulCarryUp);
+        // 繰り上げが発生したら次へ回す
+        mulCarryUp = mulResult.nextCarryUp;
+
+        // 最終結果の加算処理
+        const addResult = addPlace(
+            mulResult.val,
+            tail?.next?.val ?? 0,
+            addCarryUp
+        );
+        // 繰り上げが発生したら次へ回す
+        addCarryUp = addResult.nextCarryUp;
+
+        // 最終結果のnodeを作成し、tailを次の位へ移動
+        const node = new ListNode(addResult.val);
+        tail = updateTail(tail, node);
+
+        // 次のAを計算対象とする
+        nodeA = nodeA?.next ?? null;
+    }
+    return mulCarryUp;
+};
+
+// 現在のtail状況を判定して、次のループに渡すtailへ更新する
+export const updateTail = (
+    tail: ListNode | null,
+    node: ListNode
+): ListNode | null => {
+    // 1回目以降
+    if (tail === null) {
+        tail = node;
+    } else if (tail.next === null) {
+        // 次の位が未計算ならnodeをそのまま採用
+        tail.next = node;
+    } else {
+        // 次の位がすでに計算済みであれば、valのみ書き換え
+        tail.next.val = node.val;
+    }
+    // 1つ進める
+    tail = tail.next;
+
+    return tail;
 };
 
 // 指定位置のNodeを取得
