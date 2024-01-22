4f256726238552f8c75956319c4c009bb3012010
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2024-01-22 23:25:04 +0900

    refacta: noise-filterin

--- src/morning/002/noise-filtering.ts
+++ src/morning/002/noise-filtering.ts
@@ -8,40 +8,44 @@
  * @space_complexity O(1)
  */
 
-enum PixelTmpStatus {
-    KEEP = 2,
-    PENDING = 1,
-    NONE = 0,
+enum PixelStatus {
+    CLEAN_BLACK = 2,
+    BLACK = 1,
+    WHITE = 0,
 }
 
-enum PixclStatus {
-    ON = 1,
-    OFF = 0,
-}
+const directionDeltas: [number, number][] = [
+    [-1, 0], // Left
+    [0, -1], // Top
+    [1, 0], // Right
+    [0, 1], // Bottom
+];
 
 export const solve = (image: number[][]): number[][] => {
     // 左上から右下へ、1pxずつ検証
     for (let row = 0; row < image.length; row++) {
         for (let col = 0; col < image[0].length; col++) {
-            image[row][col] = checkCel(image, row, col);
+            image[row][col] = checkCellStatus(image, row, col);
 
-            // 描画確定(Keep)と判定されたら、4方を再帰的に探索して、保留(Pending)を描画確定(Keep)に置き換える
-            if (image[row][col] === PixelTmpStatus.KEEP) {
-                finalizeDrawing(image, row - 1, col); // Left
-                finalizeDrawing(image, row, col - 1); // Top
-                finalizeDrawing(image, row + 1, col); // Right
-                finalizeDrawing(image, row, col + 1); // Bottom
+            // 描画確定(CLEAN_BLACK) でなければ次のループへ
+            if (image[row][col] !== PixelStatus.CLEAN_BLACK) {
+                continue;
+            }
+
+            // 描画確定(CLEAN_BLACK) なら、4方を再帰的に探索して、保留(BLACK)を描画確定(CLEAN_BLACK)へ置換
+            for (const delta of directionDeltas) {
+                finalizeDrawing(image, row + delta[0], col + delta[1]);
             }
         }
     }
 
-    // ノイズ(Pending)を取り除き(Off)、描画部分(Keep)を出力形式(On)に戻す
+    // ノイズ(BLACK)を取り除き(WHITE)、描画部分(CLEAN_BLACK)を出力形式(BLACK)に戻す
     for (let row = 0; row < image.length; row++) {
         for (let col = 0; col < image[0].length; col++) {
             image[row][col] =
-                image[row][col] === PixelTmpStatus.KEEP
-                    ? PixclStatus.ON
-                    : PixclStatus.OFF;
+                image[row][col] === PixelStatus.CLEAN_BLACK
+                    ? PixelStatus.BLACK
+                    : PixelStatus.WHITE;
         }
     }
 
@@ -49,57 +53,66 @@ export const solve = (image: number[][]): number[][] => {
 };
 
 /**
+ * 対象セルの状態を判定
+ *
  * @backtracking
  */
-const checkCel = (
+const checkCellStatus = (
     image: number[][],
     row: number,
     col: number,
     aroundCheck = true
 ): number => {
-    // image に存在しない座標(外枠)は 描画確定(Keep) とみなす
-    if (!(row in image) || !(col in image[row])) {
-        return PixelTmpStatus.KEEP;
+    // image に存在しない座標(外枠)は 描画確定(CLEAN_BLACK) とみなす
+    if (isOuter(image, row, col)) {
+        return PixelStatus.CLEAN_BLACK;
     }
 
-    const cel = image[row][col];
+    const cell = image[row][col];
 
-    // 4方のチェックが不要 or 座標の値がホワイト(None) であれば、値をそのまま返却
-    if (!aroundCheck || cel === PixelTmpStatus.NONE) {
-        return cel;
+    // 4方のチェックが不要 or 座標の値がホワイト(WHITE) であれば、値をそのまま返却
+    if (!aroundCheck || cell === PixelStatus.WHITE) {
+        return cell;
     }
 
-    // この時点で、現在座標は 保留(Pending)
+    // この時点で、現在座標は 保留(BLACK)
 
-    // 4方の値を取得（再帰的に実施しない）
-    const rect: number[] = [
-        checkCel(image, row - 1, col, false), // Left
-        checkCel(image, row, col - 1, false), // Top
-        checkCel(image, row + 1, col, false), // Right
-        checkCel(image, row, col + 1, false), // Bottom
-    ];
-
-    // 描画確定(Keep)に隣接していれば、描画確定(Keep)
-    return rect.includes(PixelTmpStatus.KEEP) ? PixelTmpStatus.KEEP : cel;
+    return isAdjacentCleanBlack(image, row, col)
+        ? PixelStatus.CLEAN_BLACK
+        : cell;
 };
 
 /**
- * 描画確定(Keep)に隣接する保留(Pending)を、描画確定(Keep)に変換する
+ * 4方の値をチェックして、外枠 又は 描画確定(CLEAN_BLACK) と隣接していれば true
+ */
+const isAdjacentCleanBlack = (image: number[][], row: number, col: number) => {
+    return directionDeltas.some(delta => {
+        const y = row + delta[0];
+        const x = col + delta[1];
+        return isOuter(image, y, x) || image[y][x] === PixelStatus.CLEAN_BLACK;
+    });
+};
+
+/**
+ * 描画確定(CLEAN_BLACK)に隣接する保留(BLACK)を、描画確定(CLEAN_BLACK)に置換
+ *
  * @backtracking
  */
 const finalizeDrawing = (image: number[][], row: number, col: number) => {
-    if (
-        !(row in image) ||
-        !(col in image[row]) ||
-        image[row][col] !== PixelTmpStatus.PENDING
-    ) {
+    if (isOuter(image, row, col) || image[row][col] !== PixelStatus.BLACK) {
         return;
     }
 
-    image[row][col] = PixelTmpStatus.KEEP;
+    image[row][col] = PixelStatus.CLEAN_BLACK;
 
-    finalizeDrawing(image, row - 1, col); // Left
-    finalizeDrawing(image, row, col - 1); // Top
-    finalizeDrawing(image, row + 1, col); // Right
-    finalizeDrawing(image, row, col + 1); // Bottom
+    for (const delta of directionDeltas) {
+        finalizeDrawing(image, row + delta[0], col + delta[1]);
+    }
+};
+
+/**
+ * 外枠判定(外枠 === CLEAN_BLACK)
+ */
+const isOuter = (image: number[][], row: number, col: number) => {
+    return !(row in image && col in image[row]);
 };
